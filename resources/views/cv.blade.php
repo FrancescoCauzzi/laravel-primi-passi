0e2022654132864c02c64dcbb0046afc98d62ccb
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-05-08 17:16:36 +0200

    feat: tentativi col bonus

---
+++ resources/views/cv.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CV (First Laravel Project)</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div class="container">
+            <h1></h1>
+            <h2></h2>
+            <p>1234 Elm Street, Anytown USA</p>
+            <p>(123) 456-7890</p>
+            <p>john.doe@email.com</p>
+            <h3>Education</h3>
+            <ul>
+                <li>Bachelor of Arts in Education, XYZ University, 2008</li>
+                <li>Master of Education, ABC College, 2012</li>
+            </ul>
+
+            <h3>Teaching Experience</h3>
+            <ul>
+                <li>High School Math Teacher, Anytown High School, 2012-Present
+                    <ul>
+                        <li>Develop and implement lesson plans for Algebra, Geometry, and Trigonometry classes</li>
+                        <li>Collaborate with other teachers to create cross-curricular projects</li>
+                        <li>Supervise and mentor student teachers and interns</li>
+                    </ul>
+                </li>
+                <li>Middle School Science Teacher, City Middle School, 2008-2012
+                    <ul>
+                        <li>Taught classes in Biology, Chemistry, and Physics</li>
+                        <li>Organized and led after-school science club</li>
+                        <li>Participated in school-wide STEM initiatives</li>
+                    </ul>
+                </li>
+            </ul>
+
+            <h3>Professional Development</h3>
+            <ul>
+                <li>Completed courses in Project-Based Learning and Differentiated Instruction</li>
+                <li>Attended conferences on Technology in the Classroom and Inclusive Education</li>
+                <li>Served as a mentor teacher for new hires and student teachers</li>
+            </ul>
+
+            <h3>Skills</h3>
+            <ul>
+                <li>Proficient in Microsoft Office, Google Suite, and educational software such as Blackboard and Canvas</li>
+                <li>Strong communication and collaboration skills</li>
+                <li>Experience with student-centered teaching approaches</li>
+            </ul>
+        </div>
+
+
+
+
+
+    </div>
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,72 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+$teachers = [
+    [
+        'name' => 'Gianfranco',
+        'surname' => 'Marconi',
+        'age' => 36,
+        'address' => '35, Gray Road, Brighton, UK',
+        'image' => 'https://thumbs.dreamstime.com/b/portrait-young-male-teacher-background-school-blackboard-teacher-s-day-knowledge-day-back-to-school-study-159722312.jpg',
+        'position' => 'Middle School Math Teacher',
+        'currentSchool' => 'St. James Middle School, Brighton',
+        'education' => 'Bachelor of Arts in Education, XYZ University',
+        'years_of_experience' => 10,
+        'teaching_experience' => [
+            'Taught high school math for 3 years',
+            'Taught high school physics for 5 years',
+            'Taught high school calculus for 2 years'
+        ],
+        'professional_development' => [
+            'Completed courses in classroom management',
+            'Attended workshops on student engagement',
+            'Participated in a summer program on technology integration'
+        ],
+        'skills' => [
+            'Strong communication skills',
+            'Ability to motivate students',
+            'Proficiency in technology'
+        ]
+    ],
+    [
+        'name' => 'Jane',
+        'surname' => 'Smith',
+        'age' => 32,
+        'address' => '88, Blue Road, Brighton, UK',
+        'image' => 'https://cdn.theasmrindex.com/uploads/thumbs/6iEQI1imVr0.jpg',
+        'position' => 'Middle School Science Teacher',
+        'currentSchool' => 'St. James Middle School, Brighton',
+        'education' => 'Master of Science in Education, ABC College',
+        'years_of_experience' => 8,
+        'teaching_experience' => [
+            'Taught middle school science for 2 years',
+            'Taught high school chemistry for 3 years',
+            'Taught high school biology for 3 years'
+        ],
+        'professional_development' => [
+            'Completed courses in inquiry-based learning',
+            'Attended workshops on project-based learning',
+            'Participated in a summer program on science education'
+        ],
+        'skills' => [
+            'Excellent interpersonal skills',
+            'Ability to engage students in learning',
+            'Expertise in science'
+        ]
+    ]
+];
 
-    return view('home');
+// Use the $teachers array inside a route definition
+Route::get('/', function () use ($teachers) {
+    return view('home', compact('teachers'));
 });
+
+Route::get('/cv', function () use ($teachers) {
+    return view('cv', compact('teachers'));
+});
+
+
+// Route::get('/cv/{index}', function ($index) use ($teachers) {
+//     $teacher = $teachers[$index];
+//     return view('cv', compact('teacher'));
+// })->name('teacher.cv');
